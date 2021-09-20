1d993b5b7212e3546cfb83833ebdc52de9f90bca
Chaoses Ib <Chaos-es@outlook.com>
2021-09-20 23:46:55 +0800

    Test: Add MultiCharsQueryPerformance/Char100

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -145,13 +145,36 @@ BOOST_FIXTURE_TEST_SUITE(MultiCharsQueryPerformance, Everythings::Everything)
 
     BOOST_AUTO_TEST_SUITE(Char)
 
-        // faster than set?
         BOOST_AUTO_TEST_CASE(Branch) {
+            test_query(*this, L"x|y");
+        }
+
+        // faster than set?
+        BOOST_AUTO_TEST_CASE(RegexBranch) {
             test_query(*this, L"regex:x|y");
         }
 
-        BOOST_AUTO_TEST_CASE(Set) {
+        BOOST_AUTO_TEST_CASE(RegexSet) {
             test_query(*this, L"regex:[xy]");
+        }
+
+    BOOST_AUTO_TEST_SUITE_END()
+
+
+    BOOST_AUTO_TEST_SUITE(Char100)
+
+        BOOST_AUTO_TEST_CASE(RegexSet) {
+            test_query(*this, L"regex:[一与业严丫丿么义乐乙也予于亏云亚亦亿仡以仪仰伊众优伛伢但佑佗余佚佣佯佻佾侑依侥俑俞俣俨俺倚偃允元兖养冤冶刈刖剡劓勇匀医卣印厂压厌原厣又友台右叶叹吁吆吖吟听吲吾呀呓员呦咏咦咬咽咿哑哕哟哩哽唁唷喁喑喝喻嗌]");
+        }
+
+        // very slow
+        BOOST_AUTO_TEST_CASE(RegexBranch) {
+            test_query(*this, L"regex:一|与|业|严|丫|丿|么|义|乐|乙|也|予|于|亏|云|亚|亦|亿|仡|以|仪|仰|伊|众|优|伛|伢|但|佑|佗|余|佚|佣|佯|佻|佾|侑|依|侥|俑|俞|俣|俨|俺|倚|偃|允|元|兖|养|冤|冶|刈|刖|剡|劓|勇|匀|医|卣|印|厂|压|厌|原|厣|又|友|台|右|叶|叹|吁|吆|吖|吟|听|吲|吾|呀|呓|员|呦|咏|咦|咬|咽|咿|哑|哕|哟|哩|哽|唁|唷|喁|喑|喝|喻|嗌");
+        }
+
+        // very slow
+        BOOST_AUTO_TEST_CASE(Branch) {
+            test_query(*this, L"一|与|业|严|丫|丿|么|义|乐|乙|也|予|于|亏|云|亚|亦|亿|仡|以|仪|仰|伊|众|优|伛|伢|但|佑|佗|余|佚|佣|佯|佻|佾|侑|依|侥|俑|俞|俣|俨|俺|倚|偃|允|元|兖|养|冤|冶|刈|刖|剡|劓|勇|匀|医|卣|印|厂|压|厌|原|厣|又|友|台|右|叶|叹|吁|吆|吖|吟|听|吲|吾|呀|呓|员|呦|咏|咦|咬|咽|咿|哑|哕|哟|哩|哽|唁|唷|喁|喑|喝|喻|嗌");
         }
 
     BOOST_AUTO_TEST_SUITE_END()
