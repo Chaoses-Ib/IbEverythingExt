9ff30e27c04af0b6595927ba5ec8597168d9e407
Chaoses-Ib <Chaos-es@outlook.com>
2025-07-02 07:48:53 +0800

    feat(publish): add (#85)

---
+++ publish/publish.ps1
@@ -0,0 +1,32 @@
+﻿# Bump version
+# Build VS EverythingExt, loader, Updater
+
+cargo build -p plugin -r
+if (!$?) {
+    Write-Host "Build failed"
+    exit 1
+}
+
+rm -r target/publish
+
+mkdir target/publish/Everything/Plugins
+cp target/release/IbEverythingExt.dll target/publish/Everything/Plugins/IbEverythingExt.dll
+
+cp -r publish/IbEverythingExt target/publish/Everything/Plugins
+cp x64/Release/Updater.exe target/publish/Everything/Plugins/IbEverythingExt/Updater.exe
+
+cp x64/Release/WindowsCodecs.dll target/publish/Everything/WindowsCodecs.dll
+
+# Read version from Cargo.toml
+$cargoToml = Get-Content "Cargo.toml" -Raw
+$version = ($cargoToml | Select-String 'version = "([^"]+)"').Matches[0].Groups[1].Value
+
+# Save version to target/publish/v
+$version | Out-File -FilePath "target/publish/v" -Encoding utf8 -NoNewline
+
+pushd target/publish
+zip -r "IbEverythingExt v$version.zip" Everything
+popd
+
+.\publish\publish-14.ps1
+.\publish\publish-15.ps1
