702eb0e59501c62bbc39e739f289b6956fa11d11
Chaoses-Ib <Chaos-es@outlook.com>
2025-07-02 09:46:07 +0800

    chore: bump version

--- Updater/updater.cpp
+++ Updater/updater.cpp
@@ -39,7 +39,7 @@ std::string truncate_lines(std::string s, size_t lines) {
 
 bool check_for_update(bool prerelease, bool quiet, bool silent_error) {
     try {
-        GitHubUpdater updater{ "Chaoses-Ib", "IbEverythingExt", "v0.6-alpha.5" };
+        GitHubUpdater updater{ "Chaoses-Ib", "IbEverythingExt", "v0.7.0" };
         YAML::Node release = updater.check_for_new_release(prerelease);
         if (release.IsNull()) {
             if (!quiet)
